a10eed22b26864d480c602ea26ff79c1792bf1cf
Nurlan Shaidullaev <nurlan.shaidullaev@iaau.edu.kg>
2013-02-15 16:18:37 -0800

    Initial commit

---
+++ js/minimized.other.js
@@ -0,0 +1,31 @@
+function adjustStyle(){
+	
+	var width = 0;
+	
+	if(window.innerHeigth){
+		width = window.innerWidth;
+	}
+	else if(document.documentElement && document.documentElement.clientHeight){
+		width = document.documentElement.clientWidth;
+	}
+	
+	else if(document.body){
+		width = document.body.clientWidth;
+	}
+		
+	if( width < 600){
+		document.getElementById("gridStyle").setAttribute( "href", "../css/minimum.css" );
+		document.getElementById("navStyle").setAttribute( "href", "../css/minimum.css" );
+		document.getElementById("menuStyle").setAttribute( "href", "../css/minimum.css" );
+		document.getElementById("style").setAttribute( "href", "../css/minimum.css" );
+	}
+	
+	else{
+		document.getElementById("style").setAttribute( "href", "../css/style.css" );
+		document.getElementById("gridStyle").setAttribute( "href", "../css/960.css" );
+		document.getElementById("menuStyle").setAttribute( "href", "../css/menu.css" );
+		document.getElementById("navStyle").setAttribute( "href", "../css/nav.css" );
+	}
+}
+
+window.onload = adjustStyle();
